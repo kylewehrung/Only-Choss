4d7b7c946caabd8efe1aef5f580a643af6fc621a
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-03-31 09:31:11 -0600

    got login form set up

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -12,15 +12,80 @@ font-family: "cascadia";
 
 function LoginForm({ onLogin }) {
 
+    const validationSchema = yup.object({
+        username: yup.string().required(),
+        password: yup.string().required(),
+    });
 
-    return(
-        <h1>Hello?</h1>
-    )
-}
+    const formik = useFormik({
+        initialValues: {
+            username: "",
+            password: "",
+        },
+        validationSchema, 
+        onSubmit: (values, { setErrors, setSubmitting }) => {
+            setSubmitting(true);
+            fetch("/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(values),
+            })
+            .then((r) => {
+                setSubmitting(false);
+                if (r.ok) {
+                    r.json().then((user) => onLogin(user));
+                } else {
+                    r.json().then((err) => setErrors(err.errors));
+                }
+            })
+            .catch((error) => {
+                setSubmitting(false);
+                console.log(`Error: ${error}`)
+            });
+        },
+    });
 
 
-
-export default LoginForm;
+    return (
+        <form onSubmit={formik.handleSubmit}>
+          <FormField>
+            <CustomLabel htmlFor="username">Username</CustomLabel>
+            <Input
+              type="text"
+              id="username"
+              autoComplete="off"
+              value={formik.values.username}
+              onChange={formik.handleChange}
+            />
+          </FormField>
+          <FormField>
+            <CustomLabel htmlFor="password">Password</CustomLabel>
+            <Input
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+            />
+          </FormField>
+          <FormField>
+            <Button variant="fill" color="primary" type="submit">
+              {formik.isSubmitting ? "Loading..." : "Login"}
+            </Button>
+          </FormField>
+          <FormField>
+            {formik.errors &&
+            Object.values(formik.errors).map((err) => (
+              <Error key={err}>{err}</Error>
+            ))}
+          </FormField>
+        </form>
+      );
+    }
+    
+    export default LoginForm;
 
 
 
