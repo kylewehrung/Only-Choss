20d5e10208d199ff6444654505cfd09093ee7c1f
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-03-31 07:17:10 -0600

    got migrations to work, edited config file

--- server/config.py
+++ server/config.py
@@ -1,39 +1,26 @@
-from flask import Flask, render_template
-from flask_cors import CORS
+from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-import os
-from dotenv import load_dotenv
-load_dotenv()
+from sqlalchemy.orm import validates
 
-app = Flask(
-    __name__,
-    static_url_path='/',
-    static_folder='../client/build',
-    template_folder='../client/build')
-CORS(app)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app = Flask(__name__)
+app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-app.secret_key = os.environ.get('SESSION_KEY')
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 db = SQLAlchemy(metadata=metadata)
 
-bcrypt = Bcrypt(app)
 migrate = Migrate(app, db)
 db.init_app(app)
 
-@app.route('/')
-@app.route('/<int:id>')
-def index(id=0):
-    return render_template("index.html")
+bcrypt = Bcrypt(app)
 
 api = Api(app)
 
