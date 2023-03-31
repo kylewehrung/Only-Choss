6fc45a7c314fcd92b26b73531e7e9ef1fa4ee785
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-03-31 03:34:00 -0600

    tables built out in models

--- server/models.py
+++ server/models.py
@@ -4,21 +4,25 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
-from sqlalchemy_serializer import SerializerMixin
+
 
 from config import db, bcrypt
+
+
+
+
 
 class User(db.Model, SerializerMixin):
     __tablename__ = "users"
 
-    # serialize_rules = ("-")
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     _password_hash = db.Column(db.String)
-    # admin = db.Column(db.Boolean)
 
+    comments = db.relationship("Comment", backref="user")
+    users = association_proxy("comments", "user")
 
+    
 
     @validates("username")
     def validate_username(self, key, name):
@@ -44,6 +48,63 @@ class User(db.Model, SerializerMixin):
     
     def __repr__(self):
         return f"User ID: {self.id}, Username: {self.username}, Admin? {self.admin}"
+    
+
+
+
+
+class Boulder(db.Model, SerializerMixin):
+    __tablename__ = "boulders"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    grade = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String)
+    image = db.Column(db.String)
+    location_id = db.Column(db.Integer, db.ForeignKey("locations.id"))
+
+    comments = db.relationship("Comment", backref="boulder")
+    locations = db.relationship("Location", backref="boulder")
+    users = association_proxy("comments", "user")
+    
+
+    def __repr__(self):
+        return f"Boulder Id: {self.id}, Name: {self.name}, Grade: {self.grade}, Rating: {self.rating}, Description: {self.description}, Image: {self.image}, location id: {self.location_id}"
+    
+
+
+
+
+class Location(db.Model, SerializerMixin):
+    __tablename__ = "locations"
+
+    id = db.Column(db.Integer, primary_key=True)
+    state = db.Column(db.String)
+    region = db.Column(db.String)
+    area = db.Column(db.String)
+
+
+    def __repr__(self):
+        return f"Location Id: {self.id}, State: {self.state}, Region: {self.region}, Area: {self.area}"
+
+
+
+
+
+class Comment(db.Model, SerializerMixin):
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    boulder_id = db.Column(db.Integer, db.ForeignKey("boulders.id"))
+
+
+    def __repr__(self):
+        return f"Comment Id: {self.id}, Comment: {self.comment}, user id: {self.user_id}, boulder id: {self.boulder_id}"
+    
+
 
 
 
